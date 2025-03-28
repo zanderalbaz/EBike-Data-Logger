60128f1e61219d8aaf874bb97a6f593d4066cf8d
chocie-lan <chocielandvik@gmail.com>
2025-03-28 14:48:08 -0600

    empty CSV created with header and location filename

--- 495_prototype/src/Controller.java
+++ 495_prototype/src/Controller.java
@@ -2,6 +2,8 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Controller {
     private Model model; // don't need this??
@@ -11,6 +13,8 @@ public class Controller {
     private TransferConfirmWindow transferConfirmWindow;
     private EBikeDataLogger eBikeDataLogger;
     private SerialIO serialIO;
+
+    private String filename;
 
     public Controller(Model model, EBikeDataLogger eBikeDataLogger, SerialIO serialIO) { //do i need to add all of these here?
         this.eBikeDataLogger = eBikeDataLogger;
@@ -35,7 +39,6 @@ public class Controller {
                     Model.connectionValid = false;
                     System.out.println("CONNECTION FAILED");
                 }
-                //echoing
 
             }
         });
@@ -70,11 +73,20 @@ public class Controller {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(!transferWindow.locationEntry.getText().isEmpty()){ //if NOT empty, allow button press
-                    serialIO.getSerialWriter().setMessageToWrite("t"); //send 't' to serial -> hopefully will print SD data
+                    serialIO.getSerialWriter().setMessageToWrite("t"); //send 't' to serial -> hopefully will print SD data to java terminal
                     //need to test with sensor array
                     if (e.getSource() == transferWindow.transferdatabutton){
-                        String text = transferWindow.locationEntry.getText();
-                        System.out.println("Entered text: " + text); //eventually this needs to add to the CSV file for now, just print it
+                        String text = transferWindow.locationEntry.getText(); //get text from text box entry
+                        filename = text + ".csv"; //get time from RTC and add to file name
+                        System.out.println(filename);
+                        try(FileWriter writer = new FileWriter(filename)){
+                            //append Serial output (after t press) to CSV file
+                            writer.append(writeHeader());
+                            System.out.println("File created successfully!");
+                        }catch(IOException q){
+                            System.out.println("ERROR writing to csv");
+                        }
+
                         eBikeDataLogger.getCardLayout().show(eBikeDataLogger.getCardPanel(), "transferConfirmWindow");
                         eBikeDataLogger.setTitle("BLM E-bike Data Logger - Confirm Transfer");
                         serialIO.getSerialWriter().setMessageToWrite("TRANSFER SUCCESSFUL" + '\n');//must verify that the connection exists via echo
@@ -91,5 +103,49 @@ public class Controller {
             }
         });
 
+    }
+    public String writeHeader(){
+        String headerList = "";
+        String sensorType;
+        String sensorNum;
+        String dimension;
+        String stat;
+        for(int i = 0; i < 6;i++){//six sensors (Mag 1,2,3; Acc 1,2,3)
+            if (i%2 == 0){ //evens are all Acc
+                sensorType = "Acc";
+            } else{//Mag
+                sensorType = "Mag";
+            }
+            if (i == 0 || i ==1){
+                sensorNum = "1";
+            } else if (i == 2 || i ==3){
+                sensorNum = "2";
+            } else{
+                sensorNum = "3";
+            }
+            for (int j = 0; j <3; j++){ //three dimensions
+                if (j == 0){
+                    dimension = "X";
+                } else if (j == 1){
+                    dimension = "Y";
+                } else{
+                    dimension = "Z";
+                }
+                for(int k = 0; k<4; k++){//four summary stats
+                    if(k == 0){
+                        stat = "mean";
+                    } else if (k == 1) {
+                        stat = "min";
+                    } else if (k == 2) {
+                        stat = "max";
+                    } else{
+                        stat = "std";
+                    }
+                    headerList += (sensorType+dimension+sensorNum+":"+stat+", ");
+                }
+            }
+        }
+        headerList += ("Label");
+        return headerList;
     }
 }
